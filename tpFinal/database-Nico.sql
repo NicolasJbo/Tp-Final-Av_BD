398c6bb9dad4ff4f0a1878d92b97278095cd4a77
NicolasJbo <57155671+NicolasJbo@users.noreply.github.com>
2021-05-16 20:04:13 -0300

    Arreglo del Procedure Top10Clientes

--- tpFinal/database-Nico.sql
+++ tpFinal/database-Nico.sql
@@ -5,26 +5,21 @@ BEGIN
     IF( first_date > last_date ) THEN
 		SIGNAL SQLSTATE'45000' SET MESSAGE_TEXT='The entered dates are invalid';
 ELSE
-SELECT data.ClientName,
-       data.ClientLastName,
-       SUM(data.Total)AS Total
-FROM(SELECT c.id,
-            c.name AS ClientName,
-            c.last_name AS ClientLastName,
-            r.id AS ResidenceId,
-            e.id AS Energy,
-            MAX(m.total) - IF(COUNT(e.id) <> 1,MIN(m.total),0)  AS Total
-     FROM clients c
-              JOIN residences r
-                   ON c.id= r.id_client
-              JOIN energy_meters e
-                   ON r.id_energy_meter = e.id
-              JOIN measures m
-                   ON m.id_residence = r.id
-     WHERE DATE BETWEEN first_date AND last_date
-     GROUP BY c.id,c.name,c.last_name,r.id,energy)DATA
-GROUP BY data.ClientName
-ORDER BY Total DESC
+SELECT c.name,c.last_name,
+       SUM(p.Final)AS TotalConsumo
+FROM clients c
+         JOIN(
+    SELECT m.total,m.id_residence ,r.id_client,
+           SUM(m.total) AS Final
+    FROM measures m
+             JOIN
+         residences r
+         ON m.id_residence = r.id
+    WHERE m.date BETWEEN first_date AND last_date
+    GROUP BY m.total,m.id_residence,r.id_client
+    ORDER BY m.id_residence ASC)p
+             ON c.id=p.id_client
+GROUP BY c.name,c.last_name
     LIMIT 0,10;
 END IF;
 END;
