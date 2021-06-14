18ecfbb71dff50bc68b2bcffc9d3657112d7d0b2
LautaroFullone <lautarofullone@gmail.com>
2021-06-14 20:09:08 -0300

    Proceso Facturacion 100%

--- tpFinal/tp-items.sql
+++ tpFinal/tp-items.sql
@@ -10,13 +10,12 @@ BEGIN
     ON  r.id_tariff = t.id
     WHERE r.id = idResidence
     LIMIT 1;
+    
 END;
 //
 
-CALL getMeasurePrice(1,@hola);
-SELECT @hola
+#-------------------------------->> GENERAR FACTURA DE AJUSTE POR TARIFA ACTUALIZADA  <<-------------------
 
-#Actualiza el precio de la medicion
 DROP TRIGGER IF EXISTS tbi_MeasurePrice; 
 DELIMITER //
 CREATE TRIGGER tbi_MeasurePrice BEFORE INSERT ON measures FOR EACH ROW
@@ -42,6 +41,7 @@ BEGIN
     END IF;
 END;
 //
+#-------------------------------->> GENERAR FACTURA DE AJUSTE POR TARIFA ACTUALIZADA  <<-------------------
 
 
 #----- [PUNTO 2] Generar una factura ---------------------------------------------------------------------------------------------
@@ -50,7 +50,7 @@ DROP PROCEDURE IF EXISTS getResidenceTariffId;
 DELIMITER //
 CREATE PROCEDURE getResidenceTariffId(IN idResidence INT, OUT id INT)
 BEGIN 
-    
+
     SELECT t.id INTO id FROM tariffs AS  t
     JOIN residences AS r
     ON  r.id_tariff = t.id
@@ -78,6 +78,7 @@ DROP PROCEDURE IF EXISTS getFinalDateAndMedition;
 DELIMITER //
 CREATE PROCEDURE getFinalDateAndMedition(IN idResidence INT, OUT dateMedition DATETIME, OUT medition FLOAT)
 BEGIN 
+
     SELECT m.date, m.kw INTO dateMedition, medition
     FROM measures AS m
     INNER JOIN residences AS r
@@ -85,14 +86,15 @@ BEGIN
     WHERE (m.id_residence = idResidence) AND (m.id_bill=0)
     ORDER BY m.date DESC
     LIMIT 1;
+    
 END;
 //
-
 
 DROP PROCEDURE IF EXISTS getInitialDateAndMedition;
 DELIMITER //
 CREATE PROCEDURE getInitialDateAndMedition(IN idResidence INT, OUT dateMedition DATETIME, OUT medition FLOAT)
 BEGIN 
+
     SELECT m.date, m.kw INTO dateMedition, medition
     FROM measures AS m
     INNER JOIN residences AS r
@@ -100,6 +102,7 @@ BEGIN
     WHERE (m.id_residence = idResidence) AND (m.id_bill=0)
     ORDER BY m.date ASC
     LIMIT 1;
+    
 END;
 //
 
@@ -107,13 +110,16 @@ DROP PROCEDURE IF EXISTS getCountMeditions;
 DELIMITER //
 CREATE PROCEDURE getCountMeditions(IN idResidence INT, OUT totalCount INT)
 BEGIN 
+
     SELECT COUNT(*) INTO totalCount 
     FROM measures 
     WHERE ((id_residence = idResidence) AND (id_bill=0));
+    
 END;
 //
 
-SELECT * FROM measures;
+
+#--------------------------------------->> GENERAR UNA UNICA FACTURA  <<-----------------------------------
 
 DROP PROCEDURE IF EXISTS generateBill;
 DELIMITER //
@@ -121,7 +127,7 @@ CREATE PROCEDURE generateBill(IN idResidence INT)
 BEGIN 
     DECLARE existsResidence INT DEFAULT 1;
     DECLARE existsMeasures INT DEFAULT 0;
-
+    
     DECLARE vFinalDate DATETIME ;
     DECLARE vFinalMedition FLOAT DEFAULT 0 ; 
     DECLARE vInitialDate DATETIME ;
@@ -186,21 +192,59 @@ END;
 //
 
 CALL generateBill(2);
+#--------------------------------------->> GENERAR UNA UNICA FACTURA  <<-----------------------------------
 
 INSERT INTO measures(DATE, kw, id_residence) VALUES("2021-06-13 00:00:00 000000", 5, 1), ("2021-06-13 00:05:00 000000", 10, 1);
 INSERT INTO measures(DATE, kw, id_residence) VALUES("2021-06-13 00:10:00 000000", 20, 1), ("2021-06-13 00:15:00 000000", 45, 1);
-INSERT INTO measures(DATE, kw, id_residence) VALUES("2021-06-13 00:00:00 000000", 10, 2), ("2021-06-13 00:00:00 000000", 15, 2);
+INSERT INTO measures(DATE, kw, id_residence) VALUES("2021-06-13 00:00:00 000000", 10, 2), ("2021-06-13 00:05:00 000000", 15, 2);
 INSERT INTO measures(DATE, kw, id_residence) VALUES("2021-07-13 00:00:00 000000", 50, 1),("2021-07-13 00:12:00 000000", 72, 1) ;
-INSERT INTO measures(DATE, kw, id_residence) VALUES("2021-06-13 00:00:00 000000", 25, 2), ("2021-06-13 00:00:00 000000", 35, 2);
+INSERT INTO measures(DATE, kw, id_residence) VALUES("2021-08-13 00:10:00 000000", 25, 2), ("2021-06-13 00:30:00 000000", 35, 2);
 
 SELECT * FROM bills;                
 SELECT * FROM measures;                
                                
 DELETE FROM bills;
 DELETE FROM measures;                
-                
-                
-                    
+
+#------------------------------------------>> GENERAR TODAS LAS FACTURA  <<---------------------------------
+DROP PROCEDURE IF EXISTS generateAllBills;
+DELIMITER //
+CREATE PROCEDURE generateAllBills()
+BEGIN 
+	DECLARE vResidenceId INT DEFAULT 0; 
+	
+	DECLARE vContinue INT DEFAULT 1;
+	DECLARE residencesCursor CURSOR FOR (SELECT id FROM residences);
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET vContinue = 0;
+
+	OPEN residencesCursor;
+    
+	get_residences: LOOP
+		FETCH residencesCursor INTO vResidenceId;
+		IF(vContinue = 0) THEN
+			LEAVE get_residences;
+		END IF;
+		CALL generateBill(vResidenceId);
+	END LOOP get_residences;
+	
+	CLOSE residencesCursor; 
+END;
+//
+
+CALL generateAllBills();                
+
+#------------------------------------------>> GENERAR UNA FACTURA UNICA <<---------------------------------
+
+
+#--------------------------------->> REALIZAR LLAMADO UNA VEZ AL MES <<------------------------------------                
+DELIMITER //
+CREATE EVENT billingDay ON SCHEDULE EVERY 1 MONTH
+STARTS NOW() DO
+BEGIN 
+	CALL generateAllBills();
+END;
+//                    
+#--------------------------------->> REALIZAR LLAMADO UNA VEZ AL MES <<------------------------------------                
 
 
 
